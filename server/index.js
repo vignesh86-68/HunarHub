9c384b89b657c436586fa573add8413129ab647e
vignesh86-68 <vigneshsakthivel8668@gmail.com>
2026-08-21 23:16:57 +0530

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -52,7 +52,8 @@ app.use((err, req, res, next) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
